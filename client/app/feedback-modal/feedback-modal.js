e523a44bfe05d70098dad7a1590793c0b7b30dea
Fotis Adamakis <fotis316@yahoo.com>
2016-07-03 20:57:08 +0300

    Feedback modal

---
+++ client/app/feedback-modal/feedback-modal.js
@@ -0,0 +1,21 @@
+Template.feedbackModal.events({
+    'submit .feedbackModalForm': function(event, templateInstance) {
+        event.preventDefault();
+        let text = templateInstance.find(".feedback-textarea").value;
+        if(text){
+            $('#feedbackModal').modal('hide');
+            Meteor.call('sendEmail', null, 'remotescrum@gmail.com', 'Feedback form', text, function(err){
+                if(err){
+                    $.bootstrapGrowl('Ooops. Please try contacting us directly remotescrum@gmail.com',{
+                        type: 'danger'
+                    });
+                }else{
+                    templateInstance.find(".feedback-textarea").value = '';
+                    $.bootstrapGrowl('Thank you! We will get back to you as soon as possible.',{
+                        type: 'success'
+                    });
+                }
+            });
+        }
+    }
+});
