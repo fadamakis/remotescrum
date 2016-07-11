2eeb5ebad5b5c09af9b5af36eaba1a08b47aa0ab
Fotis Adamakis <fotis316@yahoo.com>
2016-07-11 21:12:41 +0300

    Landing page improved

--- client/app/router.js
+++ client/app/router.js
@@ -1,14 +1,14 @@
 FlowRouter.route('/retrospective/:_id', {
     name: 'sprint.show',
     action: function() {
-        BlazeLayout.render("mainLayout", {content: "board", footer: "boardFooter"});
+        BlazeLayout.render("mainLayout", {content: "board", header: "header", footer: "boardFooter"});
     }
 });
 
 FlowRouter.route('/sprints', {
     action: function() {
         if(Meteor.userId()) {
-            BlazeLayout.render("mainLayout", {content: "sprints"});
+            BlazeLayout.render("mainLayout", {content: "sprints", header: "header"});
         } else {
             FlowRouter.go("/");
         }
