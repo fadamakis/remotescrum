376a2cbc45c940a9bcd742515c882d503262db6e
Fotis Adamakis <fotis316@yahoo.com>
2016-07-20 23:20:45 +0300

    Revert "Added useless meteor packages ongoworks:spiderable and meteorhacks:fast-render"
    This reverts commit c040358ae8ff5b894acd82856090d1ca948edf8b.

--- client/app/router.js
+++ client/app/router.js
@@ -2,8 +2,7 @@ FlowRouter.route('/retrospective/:_id', {
     name: 'sprint.show',
     action: function() {
         BlazeLayout.render("mainLayout", {content: "board", header: "header", footer: "boardFooter"});
-    },
-    fastRender: true
+    }
 });
 
 FlowRouter.route('/sprints', {
@@ -19,6 +18,5 @@ FlowRouter.route('/sprints', {
 FlowRouter.route('/', {
     action: function() {
         BlazeLayout.render("mainLayout", {content: "landing", footer: "landingFooter"});
-    },
-    fastRender: true
+    }
 });
