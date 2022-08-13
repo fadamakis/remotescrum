c031054eb1f08217af456fef60f0772f5188bae9
Fotis Adamakis <fotis316@yahoo.com>
2022-08-13 15:49:47 +0200

    turn off autocomplete for new notes

--- client/app/board/board.js
+++ client/app/board/board.js
@@ -7,10 +7,10 @@ import { NotesSorting } from './notesSorting'
 const selectedNote = new ReactiveVar();
 const notesSorter = new NotesSorting()
 
-Template.board.onCreated( function() {
+Template.board.onCreated(function () {
     let template = Template.instance();
     var self = this;
-    self.autorun(function() {
+    self.autorun(function () {
         let sprintId = FlowRouter.getParam('_id');
         self.subscribe('currentSprint', sprintId);
         self.subscribe('notes', sprintId);
@@ -23,11 +23,11 @@ Template.board.helpers({
             "_id": "good",
             "name": "What went well",
             "order": 1
-        },{
+        }, {
             "_id": "bad",
             "name": "What didn't go well",
             "order": 2
-        },{
+        }, {
             "_id": "actions",
             "name": "Action items",
             "order": 3
@@ -47,22 +47,22 @@ Template.board.helpers({
 
 Template.notesTemplate.helpers({
     notes() {
-        const notes = Notes.find({sprintId : FlowRouter.getParam('_id')}).fetch();
+        const notes = Notes.find({ sprintId: FlowRouter.getParam('_id') }).fetch();
 
         return notesSorter.sort(notes, this.categoryId)
     },
-    haveVoted(note){
+    haveVoted(note) {
         let username = localStorage.getItem('username');
-        return note.voters.indexOf(username) > -1 ? 'active': '';
+        return note.voters.indexOf(username) > -1 ? 'active' : '';
     },
-    showEdit(){
+    showEdit() {
         return this.addedBy === localStorage.getItem('username')
     },
-    highlight(note){
+    highlight(note) {
         let voters = note.voters.length;
         if (voters > 4) {
             return 'highlight--3';
-        } else if(voters > 2) {
+        } else if (voters > 2) {
             return 'highlight--2';
         } else {
             return 'highlight--1';
@@ -73,7 +73,7 @@ Template.notesTemplate.helpers({
 
 function submitNote(target) {
     let text = target.value.trim();
-    if(!text) return;
+    if (!text) return;
     let categoryId = target.attributes['category-id'].value;
     let sprintId = FlowRouter.getParam('_id');
     let username = localStorage.getItem('username');
@@ -84,37 +84,38 @@ function submitNote(target) {
 
 Template.board.events({
     'keydown .new-note'(event) {
-        if(event.which === 13 || event.which === 9){ // enter or tab
+        if (event.which === 13 || event.which === 9) { // enter or tab
             event.preventDefault();
             submitNote(event.target)
         }
     },
-    'submit form.submit-note'(event) {
+    'click .submit-note button'(event) {
         event.preventDefault();
-        const target = event.target[0]
-        submitNote(target)
+        const target = $(event.currentTarget).parent().siblings('.new-note')
+        var submitEvent = $.Event("keydown", { which: 13 });
+        $(target).trigger(submitEvent)
     },
-    'dblclick .well': function(event) {
+    'dblclick .well': function (event) {
         event.preventDefault();
         selectedNote.set(this);
         $('#modal').modal('show');
     },
-    'dblclick .vote': function(event) {
+    'dblclick .vote': function (event) {
         event.stopPropagation();
         event.preventDefault();
         return false;
     },
-    'click .edit': function(event) {
-      event.preventDefault();
-      selectedNote.set(this);
-      $('#modal').modal('show');
+    'click .edit': function (event) {
+        event.preventDefault();
+        selectedNote.set(this);
+        $('#modal').modal('show');
     },
-    'click .vote': function(event) {
+    'click .vote': function (event) {
         event.stopPropagation();
         let username = localStorage.getItem('username');
         Meteor.call('notes.vote', this, username);
     },
-    'click .sort': function(event) {
+    'click .sort': function (event) {
         const categoryId = event.target.attributes['category-id'].value;
         notesSorter.nextSortDirection(categoryId)
     }
@@ -124,25 +125,25 @@ Template.modalTemplate.helpers({
     selectedNote() {
         return selectedNote.get();
     },
-    moment(createdAt){
+    moment(createdAt) {
         return moment(createdAt).fromNow();
     },
-    showRemove(owner){
+    showRemove(owner) {
         return owner === localStorage.getItem('username') || Meteor.user();
     }
 });
 
 Template.modalTemplate.events({
-    'click .saveNote': function(event, templateInstance) {
+    'click .saveNote': function (event, templateInstance) {
         event.stopPropagation();
         let note = selectedNote.get();
         let newText = templateInstance.find("textarea").value.trim();
-        if(!newText) return;
+        if (!newText) return;
         let username = localStorage.getItem('username');
         Meteor.call('notes.update', note, newText, username);
         $('#modal').modal('hide');
     },
-    'click .deleteNote': function(event) {
+    'click .deleteNote': function (event) {
         event.stopPropagation();
         let note = selectedNote.get();
         Meteor.call('notes.remove', note);
